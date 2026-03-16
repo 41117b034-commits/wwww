210ac7c5ce4548e68fe0b0f1669c80041a21a6eb
41117b034-commits <41117b034@gms.ndhu.edu.tw>
2026-03-16 23:54:01 +0800

    修改

--- wwww/Assets/Scenes/tiao.cs
+++ wwww/Assets/Scenes/tiao.cs
@@ -16,9 +16,23 @@ public class tiao : MonoBehaviour
     {
         
     }
-
+    // 跳轉場景的功能
     public void Jump()
     {
         SceneManager.LoadScene(1);
+    }
+
+    // 退出遊戲的功能
+    public void QuitGame()
+    {
+        Debug.Log("點擊了退出按鈕");
+
+#if UNITY_EDITOR
+        // 如果在編輯器內執行，就停止播放模式
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // 如果是編譯後的程式，就關閉遊戲
+            Application.Quit();
+#endif
     }
 }
