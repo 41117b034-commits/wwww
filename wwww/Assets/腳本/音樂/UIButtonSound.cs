51ea51b0d1d475919e5639b0ac4651250ed5918d
41117b034-commits <41117b034@gms.ndhu.edu.tw>
2026-07-24 13:21:46 +0800

    按鈕音效

--- wwww/Assets/腳本/音樂/UIButtonSound.cs
+++ wwww/Assets/腳本/音樂/UIButtonSound.cs
@@ -3,24 +3,39 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class UIButtonSound : MonoBehaviour
 {
-    [Header("按鈕點擊音效")]
     public AudioClip clickSound;
 
+    private static UIButtonSound instance;
     private AudioSource audioSource;
 
     private void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        // 切換場景時不要刪除 UIAudio
+        DontDestroyOnLoad(gameObject);
+
         audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.loop = false;
+        audioSource.spatialBlend = 0f;
     }
 
     public void PlayClickSound()
     {
         if (clickSound == null)
         {
-            Debug.LogWarning("尚未設定按鈕音效！");
+            Debug.LogError("UIAudio 的 Click Sound 沒有放音效！");
             return;
         }
 
         audioSource.PlayOneShot(clickSound);
+        Debug.Log("播放按鈕音效");
     }
 }
